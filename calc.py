a1feb7a636c0c7efcc8df4ec8786993ddc9d71a7
Sarvesh Patil <sarveshp273@gmail.com>
2020-10-16 20:26:33 +0530

    Button functions

---
+++ calc.py
@@ -0,0 +1,81 @@
+from tkinter import *
+import tkinter.messagebox
+import math
+
+root = Tk()
+root.geometry("650x400+300+300")
+
+root.title("Advanced Calculator and Converter")
+
+switch = None
+
+#Button Functions
+
+def btn1_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '1')
+
+
+def btn2_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '2')
+
+
+def btn3_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '3')
+
+
+def btn4_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '4')
+
+
+def btn5_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '5')
+
+
+def btn6_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '6')
+
+
+def btn7_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '7')
+
+
+def btn8_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '8')
+
+
+def btn9_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '9')
+
+
+def btn0_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '0')
